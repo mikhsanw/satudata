bc63e6a25515dea37a8b20591a5e6ca6217f03d0
herimaulana <herimaulana85@gmail.com>
2023-01-17 14:33:34 +0700

    Perubahan Frontend

---
+++ resources/views/frontend/partials/app.blade.php
@@ -0,0 +1,9 @@
+<!--header-->
+@include('frontend.partials.header')
+<!--/header-->
+
+      @yield('content')
+
+<!--Footer-->
+@include('frontend.partials.footer')
+<!--/footer-->
---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,45 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="container">
+    <h3>Bidang Statistik</h3>
+    <p>Bidang Statistik Dinas Komunikasi Informatika dan Statistik Kabupaten Bengkalis Memiliki Fungsi Sebagai Satu Data Sektoral Kabupaten Bengkalis dan juga sebagai penyebarluasan data, hal ini sesuai dengan Perpres no 39 Tahun 2019 tentang Satu Data Indonesia serta di perkuat dengan adanya Perbup Bupati Bengkalis no 5 tahun 2019. Bidang Statistik Dinas Komunikasi Informatika dan Statistik Kabupaten Bengkalis yang saat ini dipimpin oleh Bapak Azmar, S.Kom, MIP.</p>
+    <h4>"TERWUJUDNYA KABUPATEN BENGKALIS YANG BERMARWAH, MAJU DAN SEJAHTERA"</h4>
+    <br>
+    <div class="social-links">
+      <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+      <a href="#" class="youtube"><i class="bx bxl-youtube"></i></a>
+    </div>
+    <div class="copyright">
+      &copy; Copyright <strong><span>2022</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: [license-url] -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
+      Designed by <a href="https://bengkaliskab.go.id/">TIM IT Diskominfotik Kabupaten Bengkalis</a>
+    </div>
+  </div>
+</footer><!-- End Footer -->
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{asset('MyResume/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/aos/aos.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/typed.js/typed.min.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+<script src="{{asset('MyResume/assets/vendor/php-email-form/validate.js')}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('MyResume/assets/js/main.js')}}"></script>
+
+</body>
+</html>
