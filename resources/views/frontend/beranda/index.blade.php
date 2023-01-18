47cb7af1529a17571b6385fb7c3416f6888ca5bd
herimaulana <herimaulana85@gmail.com>
2023-01-18 09:52:49 +0700

    Perubahan tampilan beranda dan detail

--- resources/views/frontend/beranda/index.blade.php
+++ resources/views/frontend/beranda/index.blade.php
@@ -275,7 +275,7 @@
               </svg>
               <i class="bx bxl-dribbble"></i>
             </div>
-            <h4><a href="{{route('opdDetail', $data->id)}}">{{$data->nama}}</a></h4>
+            <h4><a href="{{route('opdDetail', $data->id)}}#detail">{{$data->nama}}</a></h4>
             <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
           </div>
         </div>
