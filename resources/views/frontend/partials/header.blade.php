61f7f3eb6c59ce58b749e9f3fb01d7ffe3721f5f
M. Ikhsan Wibowo <mikhsanw@riau.go.id>
2023-01-20 16:30:02 +0700

    penambahan pencarian

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -3,7 +3,7 @@
 
     <nav id="navbar" class="navbar nav-menu">
       <ul>
-        <li><a href="{{url('')}}#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <li><a href="{{url('')}}" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
         <li><a href="{{url('')}}#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>Satu Data</span></a></li>
         <li><a href="{{url('')}}#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Jenis Data</span></a></li>
         <li><a href="{{url('')}}#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Publikasi</span></a></li>
