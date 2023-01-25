20868c559bd1b95368e1e06f9f235c45bc775057
herimaulana <herimaulana85@gmail.com>
2023-01-25 16:34:13 +0700

    ubh ukuran image part 2

--- resources/views/frontend/beranda/index.blade.php
+++ resources/views/frontend/beranda/index.blade.php
@@ -218,7 +218,7 @@
           <div class="icon-box iconbox-blue">
             <div class="icon">
                 {{-- <i class="bx bx-file"></i> --}}
-                <img src="{{$data->file->url_stream ?? ''}}" alt="" style="width: 150px;">
+                <img src="{{$data->file->url_stream ?? ''}}" alt="" style="width: 80px;">
               </div>
             <h4><a href="{{route('opdDetail', $data->id)}}#detail">{{$data->nama}}</a></h4>
             <p>LIHAT DATA</p>
