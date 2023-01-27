045f7c55b094fd86b5bf4647c83eb0cada78648d
herimaulana <herimaulana85@gmail.com>
2023-01-27 11:17:52 +0700

    Membuat filter rentang tahun pada halaman opd

--- app/Http/Controllers/Frontend/frontendController.php
+++ app/Http/Controllers/Frontend/frontendController.php
@@ -93,11 +93,19 @@ class frontendController extends Controller
     }
 
     public function opdDetail($id){
+        $tahun5 = array(date("Y"),date("Y")-1,date("Y")-2,date("Y")-3,date("Y")-4);
+
+        if (!isset($_GET['tahunawal'], $_GET['tahunakhir'])) {
+            $tahuns = array(date("Y"),date("Y")-1,date("Y")-2,date("Y")-3,date("Y")-4);
+            }else{
+            for ($i = $_GET['tahunawal']; $i <= $_GET['tahunakhir']; $i++){
+                $tahuns[]=$i;
+            }
+        }
         $datas = Elemen::whereOpdId($id)->whereNull('parent_id')->get();
-        $tahuns = array(date("Y"),date("Y")-1,date("Y")-2,date("Y")-3,date("Y")-4);
         $elemen = new Elemen;
         $dataOpd = Opd::findOrFail($id);
-        return view('frontend.beranda.opd', compact('dataOpd','tahuns', 'elemen','datas'));
+        return view('frontend.beranda.opd', compact('dataOpd','tahuns', 'elemen','datas', 'tahun5'));
     }
 
     public function cari(Request $request){
