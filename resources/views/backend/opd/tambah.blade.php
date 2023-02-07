14108a856ef3caae4c3fa4d7bb9a3bb4f436553a
agussalim020894 <agus.andesa.fc@gmail.com>
2023-02-08 00:53:22 +0700

    perbaikan frontend final

--- resources/views/backend/opd/tambah.blade.php
+++ resources/views/backend/opd/tambah.blade.php
@@ -11,7 +11,7 @@
         </p>
         <p>
             {!! Form::label('alamat', 'Masukkan Alamat', array('class' => 'control-label')) !!}
-            {!! Form::select('alamat', null, array('id' => 'alamat', 'class' => 'form-control', 'autocomplete' => 'off')) !!}
+            {!! Form::text('alamat', null, array('id' => 'alamat', 'class' => 'form-control', 'autocomplete' => 'off')) !!}
         </p>
         <p>
             {!! Form::label('tingkatan', 'Masukkan Tingkatan', array('class' => 'control-label')) !!}
